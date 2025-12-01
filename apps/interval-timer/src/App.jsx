f005d829c52977b1464f7badb9b34b1c91534cfb
Thomas Eby <tgeby@icloud.com>
2025-12-01 12:10:32 -0500

    base turborepo with tailwind files + interval-timer vite app base

---
+++ apps/interval-timer/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-main-background-1000">
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ packages/example/src/youtube.ts
@@ -0,0 +1,1 @@
+export const youtube = () => "What up Youtube!";
