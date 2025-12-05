a3290c4d9724ad8afbdab213e1ffc6d45ff5b390
Thomas Eby <tgeby@icloud.com>
2025-12-05 09:57:54 -0500

    Changed ui tailwindconfig to compile into ES2022 for compatability with React apps instead of only Next apps

--- apps/interval-timer/src/App.jsx
+++ apps/interval-timer/src/App.jsx
@@ -1,9 +1,17 @@
+import { Gradient } from "@repo/ui/gradient";
 function App() {
 
   return (
     <>
-      <div className="min-h-screen flex flex-col items-center justify-center bg-main-background-1000">
-
+      <div className="min-h-screen flex flex-col bg-main-background-1000">
+        <p>sfld</p>
+        <div className="absolute z-50 flex items-center justify-center w-64 h-64">
+              <Gradient
+                className="opacity-90 w-[120px] h-[120px]"
+                conic
+                small
+              />
+            </div>
       </div>
     </>
   )
