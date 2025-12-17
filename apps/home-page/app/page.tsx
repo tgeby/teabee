303591c8ac44e204223b8289095649547846d682
Thomas Eby <tgeby@icloud.com>
2025-12-17 14:14:11 -0500

    Trying again. Changed vercel project config again.

--- apps/home-page/app/page.tsx
+++ apps/home-page/app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
         <Header />
         <div className="flex justify-center">
         <WelcomeUser />
-        
         </div>
         <section className="flex min-h-screen justify-center items-center">
           <a href="https://timer.teabee.org" target="_blank" rel="noopener noreferrer">Interval Timer App</a>
