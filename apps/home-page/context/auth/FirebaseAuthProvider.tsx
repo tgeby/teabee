01c8b58750a0b8a5c2b29023ae22854020132d4f
Thomas Eby <tgeby@icloud.com>
2025-12-17 18:43:23 -0500

    Changed FirebaseAuth handling to use signInWithPopup and try signInWithRedirect on failure. Sign in wasn't working on mobile with redirects. Apparently the standard approach changed recently.

--- apps/home-page/context/auth/FirebaseAuthProvider.tsx
+++ apps/home-page/context/auth/FirebaseAuthProvider.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { AuthContext, AuthUser } from "@repo/auth-contract";
 import { onAuthStateChanged, signInWithRedirect, signInWithPopup, GoogleAuthProvider, signOut, getRedirectResult } from "firebase/auth";
 import { auth } from "lib/firebase";
+import { FirebaseError } from "firebase/app";
 
 export function FirebaseAuthProvider({ children }: { children: React.ReactNode }) {
     const [user, setUser] = useState<AuthUser | null>(null);
@@ -44,15 +45,17 @@ export function FirebaseAuthProvider({ children }: { children: React.ReactNode }
                 signIn: async () => {
                     const provider = new GoogleAuthProvider();
                     try {
-                        console.log('Attempting sign-in');
-                        if (process.env.NEXT_PUBLIC_DEV === 'true') {
-                            await signInWithPopup(auth, provider);
-                        } else {
-                            await signInWithRedirect(auth, provider);
-                        }
-                        console.log('Sign-in successful');
+                        await signInWithPopup(auth, provider);
                     } catch (error) {
-                        console.error('Error during sign-in:', error);
+                        if (error instanceof FirebaseError) {
+                            console.error('Sign-in error:', error.message);
+
+                            if (error.code === 'auth/popup-blocked') {
+                                await signInWithRedirect(auth, provider);
+                            }
+                        } else {
+                            console.error('Unexpected error during sign-in:', error);
+                        }
                     }
                 },
                 signOut: async () => {
