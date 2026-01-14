0a5d7487c84e18f2114e1aca06d9dde00cf5ba20
Thomas Eby <tgeby@icloud.com>
2026-01-13 19:11:51 -0500

    Styled the TimerRunner a bit

--- apps/interval-timer/src/components/timer/TimerRunner.tsx
+++ apps/interval-timer/src/components/timer/TimerRunner.tsx
@@ -9,6 +9,8 @@ const TimerRunner = () => {
     const { id } = useParams<{ id: string }>();
     const { timer, loading, error } = useTimer(id);
     const [status, setStatus] = useState<TimerStatus>("idle");
+    const [timerIndex, setTimerIndex] = useState(0);
+    const [remainingTime, setRemainingTime] = useState<number | null>(null);
 
     if (loading) return <p>Loading timer...</p>;
     if (error || !timer) return <p>Timer not found</p>;
@@ -18,29 +20,39 @@ const TimerRunner = () => {
     const reset = () => setStatus("idle");
 
     return (
-        <div>
-            <h1>Timer Runner</h1>
-            
-             {status === "idle" && (
-                <button onClick={start}>Start</button>
-            )}
-
-            {status !== "idle" && (
-                <>
-                    <button onClick={status === "paused" ? start : pause}>
-                        {status === "paused" ? "Start" : "Stop"}
+        <>
+            <h2 className="text-4xl font-bold mb-4 text-text-bright line-clamp-2 whitespace-normal max-w-1/2 text-center">{timer.name}</h2>
+            <div className="flex flex-col items-center gap-4 font-medium text-text-bright bg-brand-primary p-6 rounded-lg">
+                {status === "idle" && (
+                    <button 
+                        onClick={start}
+                        className="text-xl sm:text-3xl bg-surface-alt p-4 rounded-lg cursor-pointer hover:bg-surface-alt/80 transition"
+                    >
+                        Start
                     </button>
+                )}
 
-                    <button onClick={reset}>
-                        Reset
-                    </button>
+                {status !== "idle" && (
+                    <>
+                        <button onClick={status === "paused" ? start : pause}>
+                            {status === "paused" ? "Start" : "Stop"}
+                        </button>
 
-                    <p>
-                        {/* countdown display */}
-                    </p>
-                </>
-            )}
-        </div>
+                        <button onClick={reset}>
+                            Reset
+                        </button>
+                    
+                    </>
+                )}
+                {/* countdown display */}
+                <p>
+                    {remainingTime !== null ? `Time Remaining: ${remainingTime} seconds` : "Not started"}
+                </p>
+                <p>
+                    Current Interval: {timerIndex + 1} / {timer.intervals.length}
+                </p>
+            </div>
+        </>
     );
 };
 
