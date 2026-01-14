7c1362ab840eed0a4a4faedfcd487cde458125f7
Thomas Eby <tgeby@icloud.com>
2026-01-13 21:09:07 -0500

    Styled the home page and make btn-glow utility class work with <a> too

--- apps/home-page/app/page.tsx
+++ apps/home-page/app/page.tsx
@@ -6,16 +6,17 @@ export default function Home() {
 
       <Header isHomePage={true} />
 
-      <main className="w-full h-screen pt-8 text-text-bright text-2xl flex justify-center">
-        <section className="w-fit h-fit p-8 rounded-lg items-center bg-brand-primary flex gap-8">
-          <span>Check out my first app...</span>
+      <main className="w-full h-screen pt-8 text-text-bright text:lg sm:text-2xl flex justify-center">
+        <section className="w-fit h-fit p-4 rounded-lg items-center flex flex-col gap-2">
+          <p>Check out my first app...</p>
+          <p className="text-center">It can help you focus and manage your time!</p>
           <a 
             href="https://timer.teabee.org" 
             target="_blank" 
             rel="noopener noreferrer"
-            className="rounded-md transition-colors cursor-pointer px-3 flex items-center gap-2 bg-surface-alt text-text-bright btn-glow h-8 self-center"
+            className="rounded-md transition-colors cursor-pointer p-2 inline-flex items-center gap-2 bg-brand-primary text-text-bright btn-glow select-none text-center text-sm sm:text-lg"
           >
-            Interval Timer App
+            Interval Timer
           </a>
         </section>
       </main>
