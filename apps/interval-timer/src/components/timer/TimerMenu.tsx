a51b97a9857afdaf670c1a59f352394c68326ded
Thomas Eby <tgeby@icloud.com>
2026-01-13 19:09:57 -0500

    Styled the TimerMenu

--- apps/interval-timer/src/components/timer/TimerMenu.tsx
+++ apps/interval-timer/src/components/timer/TimerMenu.tsx
@@ -16,25 +16,27 @@ const TimerMenu = () => {
     if (error) return <p>{error}</p>
 
     return (
-        <div className="w-1/4">
-            <div id="new" className="w-full">
+        <div className="mt-4 max-w-3/4 sm:max-w-3/4 bg-brand-primary p-4 rounded-md flex flex-col justify-center gap-4 text-lg sm:text-2xl text-text-bright font-medium">
+            <div id="new" className="flex flex-col justify-center">
                 <button 
                     onClick = {() => navigate(`/timer/edit/new`)}
-                    className="cursor-pointer bg-surface-alt w-full rounded-full"
+                    className="rounded-md transition-colors cursor-pointer px-3 flex items-center gap-2 bg-surface-alt text-text-bright btn-glow h-8 self-center"
                 >
                     Create new timer
                 </button>
             </div>
             {timers.map((timer) => (
-                <div key={timer.id} className="flex gap-2">
-                    <p>{timer.name}</p>
+                <div key={timer.id} className="grid grid-cols-4 justify-center gap-4 text-sm sm:text-lg">
+                    <p className="line-clamp-2 whitespace-normal text-center col-span-2 max-w-64">{timer.name}</p>
                     <button 
                         onClick={() => navigate(`/timer/edit/${timer.id}`)}
+                        className="rounded-md transition-colors cursor-pointer bg-surface-alt text-text-bright btn-glow h-8 text-center px-1 sm:px-2 w-full max-w-32 place-self-center"
                     >
                         Edit
                     </button>
                     <button 
                         onClick={() => navigate(`/timer/run/${timer.id}`)}
+                        className="rounded-md transition-colors cursor-pointer bg-surface-alt text-text-bright btn-glow h-8 text-center px-1 sm:px-2 w-full max-w-32 place-self-center"
                     >
                         Run
                     </button>
